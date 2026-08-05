8b53dc746a3aa962430ec71e03ecdc97d30c762c
Shahaiar Khaled <mdkhaled199913@gmail.com>
2026-08-05 16:50:05 +0600

    Refactor BlocProvider scopes for better state management

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'core/di/dependency_injection.dart';
+import 'core/data/app_data_source.dart';
 import 'core/navigation/app_router.dart';
+import 'features/add_money/data/repositories/bank_repository_impl.dart';
+import 'features/add_money/domain/repositories/bank_repository.dart';
+import 'features/add_money/domain/usecases/get_banks.dart';
+import 'features/transactions/data/repositories/transaction_repository_impl.dart';
+import 'features/transactions/domain/repositories/transaction_repository.dart';
+import 'features/transactions/domain/usecases/transactions_usecase.dart';
+import 'features/transactions/presentation/bloc/transactions_bloc.dart';
+import 'features/transactions/presentation/bloc/transactions_event.dart';
+import 'features/wallet/data/repositories/wallet_repository_impl.dart';
+import 'features/wallet/domain/repositories/wallet_repository.dart';
+import 'features/wallet/domain/usecases/add_money_to_wallet.dart';
+import 'features/wallet/domain/usecases/get_wallet_balance.dart';
+import 'features/wallet/presentation/bloc/wallet_bloc.dart';
+import 'features/wallet/presentation/bloc/wallet_event.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DependencyInjection(
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        title: 'MobilePay',
-        routerConfig: appRouter,
+    final dataSource = AppDataSource();
+
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<WalletRepository>(
+          create: (_) => WalletRepositoryImpl(dataSource),
+        ),
+        RepositoryProvider<TransactionRepository>(
+          create: (_) => TransactionRepositoryImpl(dataSource),
+        ),
+        RepositoryProvider<BankRepository>(
+          create: (_) => BankRepositoryImpl(dataSource),
+        ),
+      ],
+      child: Builder(
+        builder: (context) {
+          return MultiRepositoryProvider(
+            providers: [
+              RepositoryProvider<GetWalletBalance>(
+                create: (_) =>
+                    GetWalletBalance(context.read<WalletRepository>()),
+              ),
+              RepositoryProvider<AddMoneyToWallet>(
+                create: (_) =>
+                    AddMoneyToWallet(context.read<WalletRepository>()),
+              ),
+              RepositoryProvider<TransactionsUseCase>(
+                create: (_) =>
+                    TransactionsUseCase(context.read<TransactionRepository>()),
+              ),
+              RepositoryProvider<GetBanks>(
+                create: (_) => GetBanks(context.read<BankRepository>()),
+              ),
+            ],
+            child: Builder(
+              builder: (context) {
+                return MultiBlocProvider(
+                  providers: [
+                    BlocProvider(
+                      create: (context) => WalletBloc(
+                        getWalletBalance: context.read<GetWalletBalance>(),
+                      )..add(const WalletStarted()),
+                    ),
+                    BlocProvider(
+                      create: (context) => TransactionsBloc(
+                        getTransactions: context.read<TransactionsUseCase>(),
+                      )..add(const TransactionsStarted()),
+                    ),
+                  ],
+                  child: Builder(
+                    builder: (context) {
+                      return MaterialApp.router(
+                        debugShowCheckedModeBanner: false,
+                        title: 'MobilePay',
+                        routerConfig: appRouter,
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
